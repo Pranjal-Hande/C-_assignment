abfdd28b37729a62a6dc1b482fb4fe4a418f5c61
Pranjal-Hande <58593741+Pranjal-Hande@users.noreply.github.com>
2022-10-27 19:18:24 +0530

    committed assignments

---
+++ area_circumference_of_circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Circle
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Radius: ");
+        double Radius = Convert.ToDouble(Console.ReadLine());
+        double Area = Math.PI * Radius * Radius;
+        double circumferenceOfCircle = 2 * Math.PI * Radius;
+        Console.WriteLine("Area of circle: " + Area);
+        Console.WriteLine("\n\nCircumference of circle is: " + circumferenceOfCircle);
+        Console.ReadKey();
+    }
+}
+
